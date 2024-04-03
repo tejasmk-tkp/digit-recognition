dda9597eb3072efd70a177387b38c90d444db7ea
tejasmk-tkp <tejasmk.tkp@gmail.com>
2024-04-03 22:20:38 +0530

    remove unwanted lines and fix array size

--- mnist_loader.py
+++ mnist_loader.py
@@ -1,6 +1,5 @@
 import idx2numpy
 import numpy as np
-import cv2
 
 file = 'data/train-images-idx3-ubyte/train-images-idx3-ubyte'
 
@@ -10,43 +9,12 @@ train_label = idx2numpy.convert_from_file('data/train-labels-idx1-ubyte/train-la
 test_img = idx2numpy.convert_from_file('data/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
 test_label = idx2numpy.convert_from_file('data/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
 
-'''
-print(np.shape(label))
-print(label)
-
-cv2.imshow("Image", img[1])
-
-cv2.waitKey(0)
-
-if KeyboardInterrupt:
-    print("Closed by User!")
-    cv2.destroyAllWindows()
-'''
-
 train_data = np.array(train_img[10000::])
-print(np.shape(train_data))
+#print(np.shape(train_data))
 
-validation_data = np.array(train_img[::-1][10000::])
-print(np.shape(validation_data))
-
-'''
-#cv2.imshow('Image 1', train_data[0])
-cv2.imshow('Image 2', validation_data[0])
-
-cv2.waitKey(0)
-
-if KeyboardInterrupt:
-    print("Closed by User!")
-    cv2.destroyAllWindows()
-'''
-
-'''
-if train_data[0].all() == validation_data[0].all():
-    print(train_data[0])
-    print(" ")
-    print(validation_data[0])
-
-    print("It's the same")
-'''
+validation_data = np.array(train_img[::-1][50000::])
+#print(np.shape(validation_data))
 
 test_data = test_img
+
+
